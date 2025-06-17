36a0f9d1afe72393f1e4771e05aa2b72dfddd198
Liro Rodrigues <liro_rod.dev@hotmail.com>
2025-06-16 21:27:44 -0300

    updating dotfiles for nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,24 @@
 return {
   -- Add the plugin
+  -- Using Lazy
   {
-    "datsfilipe/vesper.nvim",
-    opts = {
-      transparent = true,
-    },
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "light",
+        transparent = true,
+      })
+      -- Enable theme
+      require("onedark").load()
+    end,
   },
 
   -- Configure LazyVim to use the Vesper colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vesper",
+      colorscheme = "onedark",
     },
   },
 }
